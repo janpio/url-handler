3ae9908067879203f05b8e3239ea8937a2361f5e
emon5122 <emon5122@gmail.com>
2023-05-01 05:05:01 +0600

    progress on login

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import FacebookProvider from "next-auth/providers/facebook";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const authOptions = {
+    session: {
+        strategy: "jwt",
+    },
+};
+const handler = NextAuth({
+    authOptions,
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: "text",
+                    placeholder: "Your username",
+                },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials, req) {
+                const user = {
+                    id: "1",
+                    name: "test",
+                    email: "test@test.com",
+                };
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+    ],
+    adapter: PrismaAdapter(prisma),
+});
+
+export { handler as GET, handler as POST };
