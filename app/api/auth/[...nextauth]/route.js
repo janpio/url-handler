e2721abf0ddb595e4be17b54700511e6fc098ec5
emon5122 <emon5122@gmail.com>
2023-05-08 21:17:11 +0600

    cleanups
    Signed-off-by: emon5122 <emon5122@gmail.com>

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,13 +5,13 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { compare } from "bcrypt";
 import prisma from "@/app/utils/prismaClient";
 
-const authOptions = {
-    session: {
-        strategy: "jwt",
-    },
-};
+// const authOptions = {
+//     session: {
+//         strategy: "jwt",
+//     },
+// };
 const handler = NextAuth({
-    authOptions,
+    // authOptions,
     providers: [
         CredentialsProvider({
             name: "Credentials",
