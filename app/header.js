7e663fd22b1653d8c553ffb0c19054fc61e34015
emon5122 <emon5122@gmail.com>
2023-05-08 23:03:37 +0600

    cleanups
    Signed-off-by: emon5122 <emon5122@gmail.com>

--- app/header.js
+++ app/header.js
@@ -2,9 +2,11 @@
 import Link from "next/link";
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
-export default function Header() {
+import { getServerSession } from "next-auth";
+export default async function Header() {
+    const session = await getServerSession();
     const LoginHandler = () => {
-        if (false) {
+        if (session?.user?.email) {
             return (
                 <Link href="/api/auth/signout">
                     <LogoutIcon />
@@ -21,7 +23,9 @@ export default function Header() {
     return (
         <header className="fixed top-0 left-0 right-0 z-50">
             <nav className="flex items-center justify-between bg-gradient-to-r from-red-800/70 via-red-400 to-red-800/80 backdrop-filter backdrop-blur-lg p-4">
-                <Link className="text-white font-bold" href="/">Home</Link>
+                <Link className="text-white font-bold" href="/">
+                    Home
+                </Link>
                 <LoginHandler />
             </nav>
         </header>
