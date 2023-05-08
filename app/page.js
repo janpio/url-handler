62144abadd0efe84a691f480a2f5387372a4153e
emon5122 <emon5122@gmail.com>
2023-05-08 21:13:57 +0600

    vercel analytics

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import { getServerSession } from "next-auth";
 import UrlHandler from "./component/urlHandler";
 import { urlGenerator } from "./utils/urlGenerator";
+import { Analytics } from "@vercel/analytics/react";
 export default async function Home() {
     const session = await getServerSession();
     return (
         <main>
             <p className="text-end">{session?.user?.email ?? "Guest"}</p>
             <UrlHandler fn={urlGenerator} session={session} />
+            <Analytics />
         </main>
     );
 }
