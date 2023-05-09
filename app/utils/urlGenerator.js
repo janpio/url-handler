3d1e7ad2ec2efdf41783c5b4245d8d0965e3dcc4
Istiak Hassan Emon <emon5122@gmail.com>
2023-05-09 16:37:42 +0600

    providers added
    Signed-off-by: Istiak Hassan Emon <emon5122@gmail.com>

--- app/utils/urlGenerator.js
+++ app/utils/urlGenerator.js
@@ -1,6 +1,15 @@
 "use server";
 import prisma from "@/app/utils/prismaClient";
 
+const pattern = /^(https?:\/\/)?([\w.-]+)\.([a-zA-Z]{2,6})(\/[\w.-]*)*\/?$/;
+const googleDrivePattern = /drive\.google\.com\/file\/d\/(.+?)\/.*$/;
+const dropboxPattern = /www\.dropbox\.com\/(.+?)\/(.+?)$/;
+const megaPattern = /mega\.nz\/(#!|file\/|)[!a-zA-Z0-9_-]{8,}$/;
+const onedrivePattern = /1drv\.ms\/(u\/s\/|s\/)[!a-zA-Z0-9_-]{15,}$/;
+const boxPattern = /app\.box\.com\/s\/[a-zA-Z0-9_-]+$/;
+const icloudPattern = /www\.icloud\.com\/.*$/;
+const wetransferPattern = /wetransfer\.com\/downloads\/[a-zA-Z0-9]{10}$/;
+
 export async function urlGenerator(url, session) {
     let newUrl = url;
     if (googleDrivePattern.test(url)) {
