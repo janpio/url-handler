e2252c9dc3c90615f74304d065ae3038a65608f3
emon5122 <emon5122@gmail.com>
2023-05-11 22:13:22 +0600

    JWT and drawer

--- prisma/migrations/20230511160933_user_role/migration.sql
+++ prisma/migrations/20230511160933_user_role/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('ADMIN', 'USER');
+
 -- CreateTable
 CREATE TABLE "Account" (
     "id" TEXT NOT NULL,
@@ -33,6 +36,7 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
