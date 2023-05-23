2aa23a9abe17a23cce752a99a19a395e7ad55108
emon5122 <emon5122@gmail.com>
2023-05-24 01:20:20 +0600

    I believe that tanstack/react-query is way better.

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,42 +1,90 @@
 "use client";
-import { useState, useEffect } from "react";
 import Link from "next/link";
 import baseUrl from "../lib/url";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 const Dashboard = () => {
-    const [data, setData] = useState({ allUrls: [], count: 0 });
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch(`${baseUrl}/api/url`, {
-                    cache: "no-store",
-                });
-                if (!res.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-                setData(await res.json());
-            } catch (error) {
-                console.error(error);
+    const queryClient = useQueryClient();
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["urlData"],
+        queryFn: async () => {
+            const res = await fetch(`${baseUrl}/api/url`);
+            if (!res.ok) {
+                throw new Error("Failed to fetch data");
             }
-        };
-        fetchData();
-    }, []);
-
-    const handleDeleteUrl = async (id) => {
-        try {
-            await fetch(`${baseUrl}/api/url/${id}`, { method: "DELETE" });
-            setData({
-                allUrls: data.allUrls.filter((url) => url.id !== id),
-                count: data.count - 1,
+            return await res.json();
+        },
+    });
+    const deleteUrl = useMutation({
+        mutationFn: async (id) => {
+            return await fetch(`${baseUrl}/api/url/${id}`, {
+                method: "DELETE",
             });
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    return (
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries(["urlData"]);
+        },
+    });
+    const loading = (
         <div className="flex flex-col pb-10">
-            <p className="mb-2">You have {data?.count} urls</p>
+            <div className="w-64 h-4 bg-neutral rounded-full animate-pulse mb-2" />
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Generated URL</th>
+                            <th>Actual URL</th>
+                            <th>Generated At</th>
+                            <th>Last Access</th>
+                            <th>Type</th>
+                            <th>Total Clicks</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {[1, 2, 3, 4, 5, 6].map((index) => (
+                            <tr className="hover" key={index}>
+                                <td>
+                                    <div className="w-4 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <div className="w-72 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <div className="w-72 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <div className="w-56 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <div className="w-56 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <div className="w-12 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <div className="w-4 h-4 bg-neutral rounded-full animate-pulse" />
+                                </td>
+                                <td>
+                                    <button
+                                        className="btn btn-disabled cursor-not-allowed opacity-50"
+                                        disabled
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+    const body = (
+        <div className="flex flex-col pb-10">
+            <p className="mb-2">You have {data?.count ?? 0} urls</p>
             <div className="overflow-x-auto">
                 <table className="table w-full">
                     <thead>
@@ -52,7 +100,7 @@ const Dashboard = () => {
                         </tr>
                     </thead>
 
-                    {data.allUrls.length === 0 ? (
+                    {data?.allUrls?.length === 0 ? (
                         <tbody>
                             <tr className="hover">
                                 <td>
@@ -88,7 +136,7 @@ const Dashboard = () => {
                         </tbody>
                     ) : (
                         <tbody>
-                            {data.allUrls.map((url, index) => (
+                            {data?.allUrls?.map((url, index) => (
                                 <tr className="hover" key={index}>
                                     <td>{index + 1}</td>
                                     <td>
@@ -118,7 +166,7 @@ const Dashboard = () => {
                                         <button
                                             className="btn btn-warning"
                                             onClick={() =>
-                                                handleDeleteUrl(url.id)
+                                                deleteUrl.mutate(url.id)
                                             }
                                         >
                                             Delete
@@ -132,6 +180,16 @@ const Dashboard = () => {
             </div>
         </div>
     );
+    if (isError) {
+        return (
+            <div>
+                <p>Something went wrong</p>
+            </div>
+        );
+    } else if (isLoading) {
+        return loading;
+    }
+    return <Suspense fallback={loading}>{body}</Suspense>;
 };
 
 export default Dashboard;
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,7 +1,18 @@
 "use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
 
 const Provider = ({ children }) => {
-    return <SessionProvider>{children}</SessionProvider>;
+    const [queryClient] = useState(() => new QueryClient());
+    return (
+        <SessionProvider>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </SessionProvider>
+    );
 };
 export default Provider;
